7222d1f8ee3bc428f7f84109339123617c20c916
Red <redonx99@gmail.com>
2026-03-28 18:02:48 -0400

    Page calendar by eight-week sections

--- app.js
+++ app.js
@@ -410,13 +410,13 @@ if (taskList) {
 
 calPrev.addEventListener('click', () => {
   const currentWeekStart = startOfWeek(localDayAnchor());
-  const prev = new Date(calCursor.getFullYear(), calCursor.getMonth(), calCursor.getDate() - 7, 12, 0, 0, 0);
+  const prev = new Date(calCursor.getFullYear(), calCursor.getMonth(), calCursor.getDate() - 56, 12, 0, 0, 0);
   calCursor = prev < currentWeekStart ? currentWeekStart : prev;
   renderCalendar();
 });
 
 calNext.addEventListener('click', () => {
-  calCursor = new Date(calCursor.getFullYear(), calCursor.getMonth(), calCursor.getDate() + 7, 12, 0, 0, 0);
+  calCursor = new Date(calCursor.getFullYear(), calCursor.getMonth(), calCursor.getDate() + 56, 12, 0, 0, 0);
   renderCalendar();
 });
 
