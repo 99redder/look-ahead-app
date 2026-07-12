b47c70ab5cfdef2479117ae94c9d79a6e5ff0dea
Red <redonx99@gmail.com>
2026-07-11 21:32:35 -0400

    Redesign calendar app icons

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Look Ahead Mobile
 // Auto-updates when page is opened
 
-const CACHE_NAME = 'look-ahead-mobile-v6';
+const CACHE_NAME = 'look-ahead-mobile-v7';
 const urlsToCache = [
   '/look-ahead-mobile.html',
   '/manifest.json',
